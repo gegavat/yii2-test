f919a012bd90a4985abbb782fe25cf2084614e65
Ilya <gegavat2007@rambler.ru>
2021-03-10 03:21:04 +0400

    Apple tree view

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,6 +7,7 @@ $params = array_merge(
 );
 
 return [
+    'name' => 'Яблочная админка',
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
